521b1435a05101d5e087cc4f4ba780fe3e1dcc7d
RanWeissman <ranw535@gmail.com>
2025-08-02 18:04:00 +0300

    python request - ovveride web

---
+++ auto_request.py
@@ -0,0 +1,20 @@
+import requests
+
+# Your API endpoint
+url = "http://127.0.0.1:8000/generate"  # or use "http://myapp.local:8000/" if you've mapped it
+
+# Form data
+data = {
+    "name": "Jhon Jhonson",
+    "id_number": "5353535353"
+}
+
+# File upload (adjust file path to a real image on your system)
+files = {
+    "file": ("profile_photo.jpg", open("C:\\Users\\Ran\\Desktop\\Ran\\Projects\\UniCard\\profile_photo.jpg", "rb"), "image/jpeg")
+        }
+
+response = requests.post(url, data=data, files=files)
+
+print("Status Code:", response.status_code)
+print("Response Text:", response.text)
